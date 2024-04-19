96bf8f4a7cb0d9201f8157ae4f3987270a91ceb9
platiins <valerijaaivanovaa@gmail.com>
2024-04-19 18:16:40 +0300

    adjustments to task 2 after i finally got the idea of loops

--- js/task2.js
+++ js/task2.js
@@ -1,11 +1,10 @@
 // Request two numbers and find only their largest common divisor.
 
 const firstNum = Number(prompt("enter first number:"));
-const secondNum = Number(prompt("enter first number:"));
-
+const secondNum = Number(prompt("enter second number:"));
 let result;
 
-for (let i = 1; i <= firstNum; i++) {
+for (let i = 1; i <= Math.min(firstNum, secondNum); i++) {
   if (firstNum % i === 0 && secondNum % i === 0) {
     result = i;
   }
